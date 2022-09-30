9476f1dd0e7ba27a00e9ca15c0db9fbef6fdc876
AllanSam <93815162+AllanSam-codes@users.noreply.github.com>
2022-09-30 14:18:42 +0530

    style

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input")
-
-console.log(txtInput)
-
+var outputDiv = document.querySelector("#output")
 
 
 
 function clickHandler() {
-    console.log("clicked")
-    console.log("Input", txtInput.value)
+    outputDiv.innerText = "sdasfvfqefcev" + txtInput.value
 }
+
 btnTranslate.addEventListener("click", clickHandler);
